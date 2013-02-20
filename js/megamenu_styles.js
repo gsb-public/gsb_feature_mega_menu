365e9d83fb1d0f22521889340e2b1d3fbf553636
creaoy <jk@propeople.com.ua>
2013-02-20 11:57:50 -0800

    Add styling for megamenu, menu block module and second level menu for mini panels

---
+++ js/megamenu_styles.js
@@ -0,0 +1,34 @@
+(function($) {
+  jQuery.fn.qtip.styles.megamenu = {
+    background: '#272727',
+    color: '#fff',
+    overflow: 'hidden',
+    textAlign: 'left',
+    width: {
+      min: 690,
+      max: 690,
+    },
+    padding: '37px 41px 30px 25px',
+    border: {
+      width: 0,
+      radius: 0,
+      color: 'transparent',
+    },
+    title: {
+      background: '#272727',
+      fontWeight: 'bold',
+      padding: '0',
+    },
+    button: {
+      cursor: 'pointer'
+    },
+    classes: {
+      target: '',
+      tip: 'qtip-tip',
+      title: 'qtip-title',
+      button: 'qtip-button',
+      content: 'qtip-content',
+      active: 'qtip-active',
+    }
+  };
+})(jQuery);
